47fe57cb3eb3ad6188b6ded2f309f5782363212d
Filipera98 <filipesobrinho18@gmail.com>
2026-04-22 11:16:34 -0300

    refactor: ajusta exercicio de condicionais

---
+++ exercicios/condicionais.js
@@ -0,0 +1,9 @@
+const estoque = 8;
+
+if (estoque > 50) {
+  console.log("Estoque cheio");
+} else if (estoque >= 10) {
+  console.log("Estoque normal");
+} else {
+  console.log("Estoque crítico");
+}
