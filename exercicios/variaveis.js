13ce953804d25756e89b7e2b3dba60fcff761add
Filipera98 <filipesobrinho18@gmail.com>
2026-04-22 11:01:24 -0300

    feat: adiciona exercicio de variaveis

---
+++ exercicios/variaveis.js
@@ -0,0 +1,9 @@
+// Exercício de variáveis
+
+const nome = "Filipe";
+let idade = 28;
+const ativo = true;
+let saldo = 100.50;
+let cidade = "Belo Horizonte";
+
+console.log(nome, idade, ativo, saldo, cidade);
