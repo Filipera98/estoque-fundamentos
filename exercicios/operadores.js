5827fa4f7494744abc4dce5570147d61f65e8862
Filipera98 <filipesobrinho18@gmail.com>
2026-04-22 11:05:31 -0300

    feat: adiciona exercicio de operadores

---
+++ exercicios/operadores.js
@@ -0,0 +1,12 @@
+// Exercício de operadores
+
+const quantidade = 3;
+const precoUnitario = 50;
+
+const total = quantidade * precoUnitario;
+
+console.log("Total:", total);
+
+// Comparações
+console.log("É maior que 100?", total > 100);
+console.log("É igual a 150?", total === 150);
